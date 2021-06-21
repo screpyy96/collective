206d4651f9eaa345a002cf0a89be514a14fb1341
Iosif <iosifscrepy@gmail.com>
2021-06-21 11:36:31 +0100

    refactor beer card

--- src/_asset/js/load-data.js
+++ src/_asset/js/load-data.js
@@ -2,11 +2,26 @@ let beer = {
   list: [],
 };
 
+let beerObservable = new Proxy(beer, {
+  set: (target, key, value) => {
+    target[key] = value;
+    renderCardList(value);
+    return true;
+  },
+});
+
+const limit = 20;
 const renderCardList = (arr) => {
   console.log(arr);
+  const beer = document.querySelector(".beer");
+
+  beer.innerHTML = "";
+
   const grid = document.querySelector(".grid");
 
-  arr.slice(0, 10).forEach((card) => {
+  const favouriteData = JSON.parse(window.localStorage.getItem("favourite"));
+
+  arr.slice(0, limit).forEach((card) => {
     const cardElement = document.createElement("div");
     cardElement.classList.add("card");
 
@@ -25,21 +40,34 @@ const renderCardList = (arr) => {
     cardImage.classList.add("card__image");
     cardImage.style.backgroundImage = `url(${card.image_url})`;
 
+    const cardIcon = document.createElement("div");
+    cardIcon.classList.add(
+      `card__icon--${favouriteData.includes(card.id) ? "thick" : "thin"}`
+    );
+    console.log(favouriteData);
+
+    cardIcon.addEventListener("click", () => {
+      console.log(card.id);
+      const savedData =
+        JSON.parse(window.localStorage.getItem("favourite")) || [];
+      const favouriteList = favouriteData.includes(card.id)
+        ? savedData.filter((x) => x !== card.id)
+        : [...savedData, card.id];
+
+      window.localStorage.setItem(
+        "favourite",
+        JSON.stringify([...new Set(favouriteList)])
+      );
+    });
+
     cardImageWrapper.appendChild(cardImage);
+    cardImageWrapper.appendChild(cardIcon);
     cardElement.appendChild(cardName);
     cardElement.appendChild(cardDescription);
     cardElement.appendChild(cardImageWrapper);
-    grid.appendChild(cardElement);
+    beer.appendChild(cardElement);
   });
 };
-
-let beerObservable = new Proxy(beer, {
-  set: (target, key, value) => {
-    renderCardList(value);
-    target[key] = value;
-    return true;
-  },
-});
 
 export const getData = () => {
   fetch("https://api.punkapi.com/v2/beers/")
